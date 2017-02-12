e0b6880e1e4aa98cdc0e1114aece390753f85559
Rohan Shukla <shuklarohan420@gmail.com>
2017-02-13 01:05:24 +0530

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,7 +1,7 @@
 #include "Stack.h"
 
 
-void StackLib::StackNew(stack *s, int elementSize)
+void stack_init(stack *s, int elementSize)
 {
 	assert(s->elementSize > 0);
 	s->elementSize = elementSize;
@@ -13,7 +13,7 @@ void StackLib::StackNew(stack *s, int elementSize)
 
 }
 
-__declspec(naked) void* StackLib::m_memcpy(void* Dst, void* src, int len)
+__declspec(naked) void* m_memcpy(void* Dst, void* src, int len)
 {
 	_asm
 	{
@@ -31,37 +31,93 @@ __declspec(naked) void* StackLib::m_memcpy(void* Dst, void* src, int len)
 
 
 
-void StackLib::StackPush(stack *s, void *elementAddress)
+void stack_push(stack *s, void *elementAddress, void(*free_function)(void*))
 {
 	if (s->logLength == s->allocLength)
-		StackLib::StackGrow(s);
+		stack_grow(s);
 
 	void *target = (char*)s->elements + s->logLength*s->elementSize;
 
-	StackLib::m_memcpy(target, elementAddress, s->elementSize);
+        m_memcpy(target, elementAddress, s->elementSize);
 	s->logLength++;
 
 
 }
 
-void StackLib::StackRemove(stack *s)
+void stack_destroy(stack *s)
 {
 	delete(s->elements); //why not just delete 's'? Think.
 }
 
-void StackLib::StackGrow(stack *s)
+void stack_grow(stack *s)
 {
 	s->allocLength *= 2;
 	s->elements = realloc(s->elements, s->allocLength*s->elementSize);
 }
 
-void StackLib::StackPop(stack *s, void* elementAddress)
+void stack_pop(stack *s, void* elementAddress)
 {
 	void *source = (char *)s->elements + (s->logLength - 1) * s->elementSize;
 
-	StackLib::m_memcpy(elementAddress, source, s->elementSize);
+	m_memcpy(elementAddress, source, s->elementSize);
 	
 	s->logLength--;
 }
 
 
+void stack_destroy(stack *s)
+{
+	if(s->free_function != ((void *)0))
+	{
+		for(int i = 0; i < s->logLength; i++)  // you have no reason to trust to ask stack to free things beyond boundaries hence you lop til log length
+		{
+			s->free_function(char *)s->elements + i*s->elementSize;
+		}
+	}
+
+	free(s->elements);
+}
+
+//stack_new(&string_stack, sizeof(char*), string_free);
+
+void string_free(void * element)
+{
+	free(*(char **)element); //what happens if we pass (char*) instead of *(char**) ? think.
+	/*
+	  if you;re storing a stack of int, doubles or even struct fractions
+	  you should pass in NULL to the stack_new last parameter
+
+	  derefercing *c the char double star is helpful, you're tranferring ownership of string back to the client.
+
+
+	*/
+
+}
+
+int main(int argc, char** argv)
+{
+	//example
+	const char* letters[] = { "tesla", "rohan", "cplusplus"};
+
+	stack string_stack;
+
+	stack_init(&string_stack, sizeof(char *)); //char* to keep track of dynamically allocated C strings
+
+	for(int i =0; i <3; i++)
+	{
+		char * copy = strdup(letters[i]);
+		stack_push(&string_stack, &copy);
+
+	}
+
+	char *name;
+	for(int i = 0; i <3; i++);
+	{
+		stack_pop(&string_stack, &name);
+		sprintf("%s\n", name);
+		free(name);
+
+	}
+	stack_destroy(&string_stack);
+
+}
